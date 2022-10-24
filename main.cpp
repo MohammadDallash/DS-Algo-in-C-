8178e91d0a38582b3e019c54a1018935189de7b7
MohammadDallash <105324962+MohammadDallash@users.noreply.github.com>
2022-10-24 14:46:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 void bubble_sort (vector <int> &v)
 {
-    
     int n = v.size();
     int s;
     
@@ -18,4 +17,44 @@ void bubble_sort (vector <int> &v)
         if (s==0) return;
     }
         
+}
+
+void quick_sort(vector<int> &v, int start, int end)
+{
+    if(start>=end) return;
+    swap(v[random(start, end)],v[end]);
+
+    int pivet  = end, p1 =start, p2 = end-1;
+
+    while (p2>p1)
+    {
+        if (v[p1] <= v[pivet])p1++;
+        if (v[p2] > v[pivet])p2++;
+        if (v[p1] > v[pivet] && v[p2] <= v[pivet]) swap(v[p1++],v[p2--]);
+    }
+    if (v[p1]>v[pivet] && p1<pivet) swap(v[p1],v[pivet]);
+    else p1++;
+
+    quick_sort(v, start, p1-1); quick_sort(v, p1, end);
+}
+
+
+
+void merge_sort(vector<int> &v, int start, int end)
+{
+    if(start==end) return;
+
+    int mid = (start+end)/2;
+    merge_sort(v,  start,  mid);  merge_sort(v,  mid+1,  end);
+
+    vector<int> temp(end-start+1,0);
+    int p1=start, p2 = mid+1,i=0;
+
+    fori(end-start+1)
+    {
+        if ((p2==end+1 || v[p1]<= v[p2] ) && p1!=mid+1)  temp[i] = v[p1++];
+        else temp[i] = v[p2++];
+    }
+
+    for(int i = start; i<= end;i++ ) v[i] = temp[i-start];
 }
